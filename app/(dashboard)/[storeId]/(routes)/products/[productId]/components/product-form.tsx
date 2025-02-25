10bdb69a1363e5966a99c3b4bfa18d43be8b5354
justinmuya <justinnjoroge426@gmail.com>
2025-02-26 00:51:22 +0300

    isArchived

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
@@ -358,7 +358,7 @@ export const ProductForm: React.FC<ProductFormProps> = ({
               control={form.control}
               name="isArchived"
               render={({ field }) => (
-                <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
+                <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4 mt-5">
                   <FormControl>
                     <Checkbox
                       checked={field.value}
