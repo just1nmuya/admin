6a04834c4345ff7f1874d2fe648fc0314ee7791f
justinmuya <justinnjoroge426@gmail.com>
2025-03-10 23:18:47 +0300

    android1

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,22 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+// import type { CapacitorConfig } from '@capacitor/cli';
+
+// const config: CapacitorConfig = {
+//   appId: 'app.vercel.adminsmokyrho',
+//   appName: 'Admin',
+//   webDir: 'public'
+// };
+
+// export default config;
+
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'app.vercel.adminsmokyrho',
   appName: 'Admin',
-  webDir: 'public'
+  webDir: 'build',
+  server: {
+    url: process.env.NEXT_PUBLIC_API_URL || 'https://admin-smoky-rho.vercel.app/',
+  }
 };
 
 export default config;
